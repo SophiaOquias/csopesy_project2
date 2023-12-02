f0f396b36183d90b803706fc9392ca9ac3c59a7e
Sophia <sophia.oquias@gmail.com>
2023-12-02 23:34:31 +0800

    IT WORKS NA FOR REAL <3 <3

--- main.py
+++ main.py
@@ -1,88 +1,70 @@
-import threading 
-import time
+from threading import Thread, BoundedSemaphore, Lock
+from time import sleep
 
-capacity = 0
-blue_waiting = 0
-green_waiting = 0
-mutex = threading.Semaphore(1)
-blue_gate = threading.Semaphore(0)
-green_gate = threading.Semaphore(0)
+n, b, g = map(int, input("Enter n, b, g values: ").split())
+room = BoundedSemaphore(value=n)
+blue_done, green_done = 0, 0
+id = 1
+switch = n
+mtx = Lock()
 
-def enter_fitting_room(color, thread_id):
-
-    global capacity, blue_waiting, green_waiting
-    mutex.acquire()
-
-    if color == "blue" and green_waiting > 0:
-        blue_waiting += 1
-        mutex.release()
-        blue_gate.acquire()
-    elif color == "green" and blue_waiting > 0:
-        green_waiting += 1
-        mutex.release()
-        green_gate.acquire()
+def fit_room(color):
+    global id, blue_done, green_done
+    print(f"ID: {id} Color: {color}")
+    if color == 'Green':
+        green_done += 1
     else:
-        capacity += 1
-        if capacity == 1:
-            if color == "blue":
-                print("Blue only.")
-            elif color == "green":
-                print("Green only.")
-        mutex.release()
-    
-    print(f"time: {time.time()} {color} thread with ID {thread_id} entered fitting room.")
-    # time.sleep(1)
+        blue_done += 1
 
-def exit_fitting_room(color, thread_id):
+    id += 1
+    sleep(1)
+    room.release()
 
-    global capacity, blue_waiting, green_waiting
-    mutex.acquire()
-    capacity -= 1
-    
-    if color == "blue" and capacity == 0:
-        while green_waiting > 0:
-            green_gate.release()
-            green_waiting -= 1
-    elif color == "green" and capacity == 0:
-        while blue_waiting > 0:
-            blue_gate.release()
-            blue_waiting -= 1
-    
-    if capacity == 0:
-        print("Empty fitting room.")
-    
-    print(f"time: {time.time()} {color} thread with ID {thread_id} exited fitting room.")
-    # time.sleep(1)
-    
-    mutex.release()
+    if room._value == n:
+        print("\nEmpty fitting room")
 
-# get input 
+def color_func(color):
+    global id, mtx, room, blue_done, green_done, switch
+    ctr = 0
 
-def create_blue_thread(thread_id): 
-    enter_fitting_room("blue", thread_id)
-    exit_fitting_room("blue", thread_id)
+    while (green_done != g and color == 'Green') or (blue_done != b and color == 'Blue'):
+        mtx.acquire()
+        print(f"\n{color} Only")
 
-def create_green_thread(thread_id): 
-    enter_fitting_room("green", thread_id)
-    exit_fitting_room("green", thread_id)
+        temp = 1
+        while temp == 1:
+            room.acquire()
+            Thread(target=lambda: fit_room(color)).start()
+            sleep(1)
 
-capacity = int(input())
-b = int(input())
-g = int(input())
+            if (green_done != g and color == 'Green') or (blue_done != b and color == 'Blue'):
+                if ctr == switch - 1:
+                    other_color_done = green_done if color == 'Blue' else blue_done
+                    if other_color_done != (g if color == 'Green' else b):
+                        while room._value != n:
+                            pass
+                        mtx.release()
+                        sleep(1)
+                        temp = 0
+                        ctr = 0
+                else:
+                    ctr += 1
+            elif (green_done == g and color == 'Green') or (blue_done == b and color == 'Blue'):
+                ctr = 0
+                while room._value != n:
+                    pass
+                mtx.release()
+                sleep(1)
+                temp = 0
 
-threads = []
+def main():
+    global n, b, g
+    green_thread = Thread(target=lambda: color_func('Green'))
+    blue_thread = Thread(target=lambda: color_func('Blue'))
 
-# Set manual IDs for blue threads
-for i in range(b):
-    thread = threading.Thread(target=create_blue_thread, args=(f"Blue_{i}",))
-    threads.append(thread)
-    thread.start()
+    green_thread.start()
+    sleep(1)
+    blue_thread.start()
 
-# Set manual IDs for green threads
-for i in range(g):
-    thread = threading.Thread(target=create_green_thread, args=(f"Green_{i}",))
-    threads.append(thread)
-    thread.start()
-
-for thread in threads:
-    thread.join()
+if __name__ == "__main__":
+    main()
