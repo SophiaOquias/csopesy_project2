196e2def9998b10747306a62436729526954d5c3
Sophia <sophia.oquias@gmail.com>
2023-12-03 01:17:57 +0800

    works

--- main.py
+++ main.py
@@ -1,78 +1,59 @@
-from threading import Thread, BoundedSemaphore, Lock
+import threading
 from time import sleep
+import random 
 
-n, b, g = map(int, input("Enter n, b, g values: ").split())
-room = BoundedSemaphore(value=n)
-blue_done, green_done = 0, 0
-id = 1
-switch = n
-mutex = Lock()
+class FittingRoom:
+    def __init__(self, n):
+        self.n = n
+        self.count = 0
+        self.color = None
+        self.lock = threading.Lock()
+        self.condition = threading.Condition(self.lock)
 
-def fit_room(color):
-    global id, blue_done, green_done
-    print(f"{id} {color}")
-    if color == 'Green':
-        green_done += 1
-    else:
-        blue_done += 1
+    def enter_room(self, thread_id, color):
+        with self.lock:
+            while self.count >= self.n or (self.color and self.color != color):
+                self.condition.wait()
 
-    id += 1
+            if self.count == 0:
+                self.color = color
+                print(f"{color} only.")
+
+            self.count += 1
+            print(f"{thread_id} {color}")
+
+    def exit_room(self, thread_id, color):
+        with self.lock:
+            self.count -= 1
+
+            if self.count == 0:
+                self.color = None
+                print("Empty fitting room.")
+
+            self.condition.notify_all()
+
+def simulate(thread_id, color, fitting_room):
+    fitting_room.enter_room(thread_id, color)
     sleep(1)
-    room.release()
-
-    if room._value == n:
-        print("\nEmpty fitting room")
-
-def color_func(color):
-    global id, mutex, room, blue_done, green_done, switch
-    ctr = 0
-
-    while (green_done != g and color == 'Green') or (blue_done != b and color == 'Blue'):
-        mutex.acquire()
-        print(f"\n{color} Only")
-
-        temp = True
-        while temp == True:
-            room.acquire()
-            Thread(target=lambda: fit_room(color)).start()
-            sleep(0.1)
-
-            if (green_done != g and color == 'Green') or (blue_done != b and color == 'Blue'):
-                if ctr == switch - 1:
-
-                    other_color_done = green_done if color == 'Blue' else blue_done
-                    total = g if color == 'Green' else b 
-
-                    if other_color_done != total:
-
-                        # while room is still occupied do nothing 
-                        while room._value != n:
-                            pass
-
-                        mutex.release()
-                        sleep(1)
-
-                        temp = False
-                        ctr = 0
-                else:
-                    ctr += 1
-            elif (green_done == g and color == 'Green') or (blue_done == b and color == 'Blue'):
-                ctr = 0
-                while room._value != n:
-                    pass
-                mutex.release()
-                sleep(1)
-                temp = False
-
-def main():
-    global n, b, g
-    
-    blue_thread = Thread(target=lambda: color_func('Blue'))
-    green_thread = Thread(target=lambda: color_func('Green'))
-
-    blue_thread.start()
-    sleep(1)
-    green_thread.start()
+    fitting_room.exit_room(thread_id, color)
 
 if __name__ == "__main__":
-    main()
+    input_values = input("Input: ")
+    n, b, g = map(int, input_values.split())
+
+    fitting_room = FittingRoom(n)
+    threads = []
+
+    for i in range(b):
+        thread = threading.Thread(target=simulate, args=(i, 'Blue', fitting_room))
+        threads.append(thread)
+
+    for i in range(g):
+        thread = threading.Thread(target=simulate, args=(i, 'Green', fitting_room))
+        threads.append(thread)
+
+    for thread in threads:
+        thread.start()
+
+    for thread in threads:
+        thread.join()
