e178007835eb2b6cc43e3cb393db95de24e91abf
Sophia <sophia.oquias@gmail.com>
2023-12-03 12:38:29 +0800

    implemented round robin turn taking

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import threading
 from time import sleep
-import random 
 
 class FittingRoom:
-    def __init__(self, n):
+    def __init__(self, n, b, g):
         self.n = n
+        self.b = b
+        self.g = g
         self.count = 0
         self.color = None
         self.lock = threading.Lock()
         self.condition = threading.Condition(self.lock)
+        self.temp_count = 0
+        self.last_color = None 
 
     def enter_room(self, thread_id, color):
         with self.lock:
-            while self.count >= self.n or (self.color and self.color != color):
+            while self.count >= self.n or (self.color and self.color != color) or (self.temp_count >= self.n and (self.b > 0 and self.g > 0)) or (color == self.last_color and (self.b > 0 and self.g > 0)):
                 self.condition.wait()
 
             if self.count == 0:
@@ -20,14 +23,22 @@ class FittingRoom:
                 print(f"{color} only.")
 
             self.count += 1
+            self.temp_count += 1
             print(f"{thread_id} {color}")
 
     def exit_room(self, thread_id, color):
         with self.lock:
             self.count -= 1
 
+            if color == 'Blue': 
+                self.b -= 1 
+            else: 
+                self.g -= 1
+
             if self.count == 0:
                 self.color = None
+                self.temp_count = 0
+                self.last_color = color 
                 print("Empty fitting room.")
 
             self.condition.notify_all()
@@ -41,7 +52,7 @@ if __name__ == "__main__":
     input_values = input("Input: ")
     n, b, g = map(int, input_values.split())
 
-    fitting_room = FittingRoom(n)
+    fitting_room = FittingRoom(n, b, g)
     threads = []
 
     for i in range(b):
